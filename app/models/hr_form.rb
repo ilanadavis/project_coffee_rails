7bb4c5f04aaaa5bb2b13fb5989e8ae45f2b056af
Ilana Davis <ilana@ilanadavis.com>
2018-04-03 19:02:18 +0000

    Add test for get and post hire forms

--- app/models/hr_form.rb
+++ app/models/hr_form.rb
@@ -1,2 +1,4 @@
 class HrForm < ApplicationRecord
+  
+  
 end
--- test/controllers/hire_forms_controller_test.rb
+++ test/controllers/hire_forms_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class HireFormsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @hr_form = hr_forms(:one)
+  end
+
+  test "should get new" do
+    get new_hire_form_url
+    assert_response :success
+  end
+  
+
+  test "should create hire_form" do
+    assert_difference('HrForm.count') do
+      post hire_forms_url, params: { annual_base_salary: @hr_form.annual_base_salary, department: @hr_form.department, email: @hr_form.email, hire_date: @hr_form.hire_date, manager_name: @hr_form.manager_name, name: @hr_form.name, phone_number: @hr_form.phone_number, title: @hr_form.title, total_annual_salary: @hr_form.total_annual_salary, variable_salary: @hr_form.variable_salary }
+    end
+
+    assert_redirected_to new_hire_form_url
+  end
+  
+  
+  
+  
+  
 end
